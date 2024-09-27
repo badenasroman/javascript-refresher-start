88eebd101190909dd858dbb74143708324404378
Josele <40341547+badenasroman@users.noreply.github.com>
2024-09-27 20:38:17 +0000

    Final Commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,82 +1,82 @@
 // Import & Export //////////
-// import def from "./util.js";
-// import { abc as content, apiKey } from "./util.js";
-// import * as util from "./util.js";
+// import def from "./util.js"
+// import { abc as content, apiKey } from "./util.js"
+// import * as util from "./util.js"
 
-// console.log(def);
-// console.log(content);
-// console.log(apiKey);
-// console.log(util.default);
+// console.log(def)
+// console.log(content)
+// console.log(apiKey)
+// console.log(util.default)
 // Import & Export //////////
 
-// Function /////////////
+// Functions /////////////
 // function greetUser(userName, message = "Hello!!!!") {
-//   // console.log(userName);
-//   // console.log(message);
-//   return "Hi, I am " + userName + ". " + message;
+//   // console.log(userName)
+//   // console.log(message)
+//   return "Hi, I am " + userName + ". " + message
 // }
 
-// console.log(greetUser("Jose L Roman"));
-// console.log(greetUser("Manuel", "What's up?"));
+// console.log(greetUser("Jose L Roman"))
+// console.log(greetUser("Manuel", "What's up?"))
 
 // export default (userName, message) => {
-//   console.log(`User: ${userName} says: ${message}`);
-// };
+//   console.log(`User: ${userName} says: ${message}`)
+// }
 
-// import logMessage from "./util";
-// logMessage("JohnDoe", "Hello, this is a test message!");
-// Function /////////////
+// import logMessage from "./util"
+// logMessage("JohnDoe", "Hello, this is a test message!")
+// Functions /////////////
 
 // Object /////////////
 // const user = {
 //   name: "Max",
 //   age: 23,
 //   greet() {
-//     console.log("Username: " + this.name);
-//     console.log("User age: " + this.age);
-//     console.log("Greeting: Hello!!!");
+//     console.log("Username: " + this.name)
+//     console.log("User age: " + this.age)
+//     console.log("Greeting: Hello!!!")
 //   },
-// };
+// }
 
-// console.log("Username: " + user.name);
-// console.log("User age: " + user.age);
-// console.log(user.greet());
+// console.log("Username: " + user.name)
+// console.log("User age: " + user.age)
+// console.log(user.greet())
 // Object /////////////
 
 // Class ////////////
 // class User {
 //   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
+//     this.name = name
+//     this.age = age
 //   }
 //   greet() {
-//     console.log("Hi");
+//     console.log("Hi")
 //   }
 // }
 
-// const user1 = new User("Manuel", 35);
-// console.log(user1);
-// user1.greet();
+// const user1 = new User("Manuel", 35)
+// console.log(user1)
+// user1.greet()
 // Class ////////////
 
 // Arrays and Methods /////////////
-// const hobbies = ["Sports", "Cooking", "Reading"];
-// console.log(hobbies[2]);
+// const hobbies = ["Sports", "Cooking", "Reading"]
+// console.log(hobbies[2])
 
-// hobbies.push("Working");
-// console.log(hobbies);
+// hobbies.push("Working")
+// console.log(hobbies)
 
 // const index = hobbies.findIndex((item) => {
-//   return item === "Working";
-// });
+//   return item === "Working"
+// })
 
-// const index = hobbies.findIndex((item) => item === "Cooking");
+// const index = hobbies.findIndex((item) => item === "Cooking")
 
-// console.log(index);
+// console.log(index)
 
-// let editedHobbies = hobbies.map((item) => item + "!");
-// let editedHobbies = hobbies.map((item) => ({ text: item }));
-// console.log(editedHobbies);
+// let editedHobbies = hobbies.map((item) => item + "!")
+// let editedHobbies = hobbies.map((item) => ({ text: item }))
+// console.log(editedHobbies)
 // Arrays and Methods /////////////
 
 // Destructuring //////////
@@ -84,71 +84,90 @@
 // const firstName = userNameData[0]
 // const lastName = userNameData[1]
 
-// const [firstName, lastName] = ["Jose", "Roman"];
-// console.log(firstName);
-// console.log(lastName);
+// const [firstName, lastName] = ["Jose", "Roman"]
+// console.log(firstName)
+// console.log(lastName)
 
 // const { name: userName, age } = {
 //   name: "Max",
 //   age: 34,
-// };
-// console.log(userName);
-// console.log(age);
+// }
+// console.log(userName)
+// console.log(age)
 // Destructuring //////////
 
 // Spread Operator /////////////
-// const hobbies = ["Sports", "Cooking", "Reading"];
-// const newHobbies = ["Working"];
+// const hobbies = ["Sports", "Cooking", "Reading"]
+// const newHobbies = ["Working"]
 
-// const mergedHobbies = [...hobbies, ...newHobbies];
-// console.log(mergedHobbies);
+// const mergedHobbies = [...hobbies, ...newHobbies]
+// console.log(mergedHobbies)
 
 // const user = {
 //   name: "Max",
 //   age: 34,
-// };
+// }
 
 // const extendedUser = {
 //   isAdmin: true,
 //   ...user,
-// };
-// console.log(extendedUser);
+// }
+// console.log(extendedUser)
 // Spread Operator /////////////
 
 // Control Structure: controls which code is executed ///////
-// const password = prompt("Your Password");
+// const password = prompt("Your Password")
 // let password
 
 // if (password === "Hello") {
-//   console.log("Hello works!");
+//   console.log("Hello works!")
 // }
 // else if(password === "Something"){
 //   console.log('Something works!')
 //}
-// const hobbies = ["Sports", "Cooking", "Reading"];
+// const hobbies = ["Sports", "Cooking", "Reading"]
 
 // for (const hobby of hobbies) {
-//   console.log(hobby);
+//   console.log(hobby)
 // }
 // Control Structure: controls which code is executed ////////
 
 // Functions as values ///////////
-function handleTimeout() {
-  console.log("Time out...1...!");
-}
+// function handleTimeout() {
+//   console.log("Time out...1...!")
+// }
 
-const handleTimeout2 = () => {
-  console.log("Time out...2...!");
-};
-setTimeout(handleTimeout, 3000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => {
-  console.log("Time out...3...!");
-}, 3000);
+// const handleTimeout2 = () => {
+//   console.log("Time out...2...!")
+// }
+// setTimeout(handleTimeout, 3000)
+// setTimeout(handleTimeout2, 3000)
+// setTimeout(() => {
+//   console.log("Time out...3...!")
+// }, 3000)
 
-function greeter(greetFn) {
-  greetFn();
-}
+// function greeter(greetFn) {
+//   greetFn()
+// }
 
-greeter(() => console.log("Hi Norman!!!"));
+// greeter(() => console.log("Hi Norman!!!"))
 // Functions as values ///////////
+
+// Functions inside Functions ///////////
+// function init() {
+//   function greet() {
+//     console.log("Hi Jose!!!")
+//   }
+//   greet()
+// }
+// init()
+// Functions inside Functions ///////////
+
+// Reference vs Primitive Vales ////////
+// let userMessage = "Hello";
+// console.log(userMessage.concat("!!!"));
+
+// const hobbies = ["Sports", "Cooking"];
+// hobbies.push("Working"); // Mutates the original array
+// console.log(hobbies);
+// Reference vs Primitive Vales ////////
